9476eb876436d9b4f13f9585d52b599654d2ef60
Thomasjkeel <thomasjames.keel@googlemail.com>
2019-05-05 14:18:25 +0100

    add new alternative globe that is draggable and add new lightwieght world json

--- assets/js/alt_globe.js
+++ assets/js/alt_globe.js
@@ -1,123 +1,347 @@
+// source: https://jorin.me/d3-canvas-globe-hover/
+// Configuration
+
+// ms to wait after dragging before auto-rotating
+var rotationDelay = 3000
+// scale of the globe (not the canvas element)
+var scaleFactor = 0.9
+// autorotation speed
+var degPerSec = 6
+// start angles
+var angles = { x: -20, y: 40, z: 0}
+// colors
+var colorWater = '#fff'
+var colorLand = '#111'
+var colorGraticule = '#ccc'
+var colorCountry = '#a00'
+
+
+//
+// Handler
+//
+
+function enter(country) {
+    var country = countryList.find(function(c) {
+        return c.id === country.id
+    })
+    current.text(country && country.name || 'Please Hover Over a Country')
+}
+
+function leave(country) {
+    current.text('Please Hover Over a Country')
+}
+
+//
+// Variables
+//
+
+var current = d3.select('#current')
+var canvas = d3.select('#globe')
+var context = canvas.node().getContext('2d')
+var water = {type: 'Sphere'}
+var projection = d3.geoOrthographic().precision(0.1)
+var graticule = d3.geoGraticule10()
+var path = d3.geoPath(projection).context(context)
+var v0 // Mouse position in Cartesian coordinates at start of drag gesture.
+var r0 // Projection rotation as Euler angles at start.
+var q0 // Projection rotation as versor at start.
+var lastTime = d3.now()
+var degPerMs = degPerSec / 1000
+var width, height
+var land, countries
+var countryList
+var autorotate, now, diff, roation
+var currentCountry
+
+//
+// Functions
+//
+
+function setAngles() {
+    var rotation = projection.rotate()
+    rotation[0] = angles.y
+    rotation[1] = angles.x
+    rotation[2] = angles.z
+    projection.rotate(rotation)
+}
+
+function scale() {
+    width = document.documentElement.clientWidth
+    height = document.documentElement.clientHeight
+    canvas.attr('width', width).attr('height', height)
+    projection
+        .scale((scaleFactor * Math.min(width, height)) / 2)
+        .translate([width / 2, height / 2])
+    render()
+}
+
+function startRotation(delay) {
+    autorotate.restart(rotate, delay || 0)
+}
+
+function stopRotation() {
+    autorotate.stop()
+}
+
+function dragstarted() {
+    v0 = versor.cartesian(projection.invert(d3.mouse(this)))
+    r0 = projection.rotate()
+    q0 = versor(r0)
+    stopRotation()
+}
+
+function dragged() {
+    var v1 = versor.cartesian(projection.rotate(r0).invert(d3.mouse(this)))
+    var q1 = versor.multiply(q0, versor.delta(v0, v1))
+    var r1 = versor.rotation(q1)
+    projection.rotate(r1)
+    render()
+}
+
+function dragended() {
+    startRotation(rotationDelay)
+}
+
+function render() {
+    context.clearRect(0, 0, width, height)
+    fill(water, colorWater)
+    stroke(graticule, colorGraticule)
+    fill(land, colorLand)
+    if (currentCountry) {
+        fill(currentCountry, colorCountry)
+    }
+}
+
+function fill(obj, color) {
+    context.beginPath()
+    path(obj)
+    context.fillStyle = color
+    context.fill()
+}
+
+function stroke(obj, color) {
+    context.beginPath()
+    path(obj)
+    context.strokeStyle = color
+    context.stroke()
+}
+
+function rotate(elapsed) {
+    now = d3.now()
+    diff = now - lastTime
+    if (diff < elapsed) {
+        rotation = projection.rotate()
+        rotation[0] += diff * degPerMs
+        projection.rotate(rotation)
+        render()
+    }
+    lastTime = now
+}
+
+function loadData(cb) {
+    d3.json('./assets/js/world-110m.json', function(error, world) {
+        if (error) throw error
+        d3.tsv('https://gist.githubusercontent.com/mbostock/4090846/raw/07e73f3c2d21558489604a0bc434b3a5cf41a867/world-country-names.tsv', function(error, countries) {
+            if (error) throw error
+            cb(world, countries)
+        })
+    })
+}
+
+// https://github.com/d3/d3-polygon
+function polygonContains(polygon, point) {
+    var n = polygon.length
+    var p = polygon[n - 1]
+    var x = point[0], y = point[1]
+    var x0 = p[0], y0 = p[1]
+    var x1, y1
+    var inside = false
+    for (var i = 0; i < n; ++i) {
+        p = polygon[i], x1 = p[0], y1 = p[1]
+        if (((y1 > y) !== (y0 > y)) && (x < (x0 - x1) * (y - y1) / (y0 - y1) + x1)) inside = !inside
+        x0 = x1, y0 = y1
+    }
+    return inside
+}
+
+function mousemove() {
+    var c = getCountry(this)
+    if (!c) {
+        if (currentCountry) {
+            leave(currentCountry)
+            currentCountry = undefined
+            render()
+        }
+        return
+    }
+    if (c === currentCountry) {
+        return
+    }
+    currentCountry = c
+    render()
+    enter(c)
+}
+
+function getCountry(event) {
+    var pos = projection.invert(d3.mouse(event))
+    return countries[0].features.find(function(f) {
+        return f.geometry.coordinates.find(function(c1) {
+            return polygonContains(c1, pos) || c1.find(function(c2) {
+                return polygonContains(c2, pos)
+            })
+        })
+    })
+}
+
+
+//
+// Initialization
+//
+
+setAngles()
+
+canvas
+    .call(d3.drag()
+        .on('start', dragstarted)
+        .on('drag', dragged)
+        .on('end', dragended)
+    )
+    .on('mousemove', mousemove)
+
+loadData(function(world, cList) {
+    land = topojson.feature(world, world.objects.land)
+    // countries = topojson.feature(world, world.objects.countries)
+    countryList = cList
+
+    window.addEventListener('resize', scale)
+    scale()
+    autorotate = d3.timer(rotate)
+})
+
+/*OTHER EXAMPLE*/
+
 // source https://bl.ocks.org/john-guerra/43c7656821069d00dcbc
-const width = 960;
-const height = 500;
-const config = {
-    speed: 0.001,
-    verticalTilt: -30,
-    horizontalTilt: 0
-}
-let locations = [];
-const svg = d3.select('svg')
-    .attr('width', width).attr('height', height);
-const markerGroup = svg.append('g');
-const projection = d3.geoOrthographic();
-const initialScale = projection.scale();
-const path = d3.geoPath().projection(projection);
-const center = [width/2, height/2];
-
-// my variables
-var rotationTimer;
-
-// Define color scale
-var color = d3.scaleLinear()
-    .domain([1, 20])
-    .clamp(true)
-    .range(['#fff', '#409A99']);
-
-// Get province name
-function nameFn(d){
-    return d && d.properties ? d.properties.ADMIN : null;
-}
-
-// Get province name length
-function nameLength(d){
-    var n = nameFn(d);
-    return n ? n.length : 0;
-}
-
-// Get province color
-function fillFn(d){
-    return color(nameLength(d));
-}
-
-
-function mouseover(d){
-    // Highlight hovered province
-    disableRotation();
-    d3.select(this).style('fill', 'orange');
-
-}
-
-function mouseout(d){
-    // Reset province color
-    // enableRotation()
-    svg.selectAll('path')
-        // TODO fix styling
-        .style('fill', '#e5e5e5'); // function(d){return fillFn(d);});
-}
-
-
-drawGlobe();
-drawGraticule();
-enableRotation();
-
-function drawGlobe() {
-    d3.queue()
-        .defer(d3.json, './assets/js/world-110m.json')
-        .defer(d3.json, './assets/js/countries.geojson')
-        .await((error, worldData, locationData) => {
-            svg.selectAll(".segment")
-                .data(topojson.feature(worldData, worldData.objects.countries).features)
-                .enter().append("path")
-                .attr("class", "segment")
-                .attr("d", path)
-                .style("stroke", "#888")
-                .style("stroke-width", "1px")
-                .style("fill", (d, i) => '#e5e5e5')
-                .style("opacity", ".7")
-                .on('mouseover', mouseover)
-                .on('mouseout', mouseout);
-            // locations = locationData;
-            // var features = countries[0].features;
-            //
-            // // Update color scale domain based on data
-            // color.domain([0, d3.max(features, nameLength)]);
-            //
-            // // Draw each province as a path
-            // svg.selectAll('path')
-            //     .data(features)
-            //     .enter().append('path')
-            //     .attr('d', path)
-            //     .attr('vector-effect', 'non-scaling-stroke')
-            //     .style('fill', fillFn)
-                // .on('mouseover', mouseover)
-                // .on('mouseout', mouseout)
-            // .on('click', clicked);
-        });
-}
-
-
-function drawGraticule() {
-    const graticule = d3.geoGraticule()
-        .step([10, 10]);
-
-    svg.append("path")
-        .datum(graticule)
-        .attr("class", "graticule")
-        .attr("d", path)
-        .style("fill", "#fff")
-        .style("stroke", "#ccc");
-}
-
-
-
-function enableRotation() {
-    rotationTimer = d3.timer(function (elapsed) {
-        projection.rotate([config.speed * elapsed - 120, config.verticalTilt, config.horizontalTilt]);
-        svg.selectAll("path").attr("d", path);
-    });
-}
-
-function disableRotation() {
-    rotationTimer.stop();
-}
+// const width = 960;
+// const height = 500;
+// const config = {
+//     speed: 0.01,
+//     verticalTilt: -30,
+//     horizontalTilt: 0
+// }
+// let locations = [];
+// const svg = d3.select('svg')
+//     .attr('width', width).attr('height', height);
+// const markerGroup = svg.append('g');
+// const projection = d3.geoOrthographic();
+// const initialScale = projection.scale();
+// const path = d3.geoPath().projection(projection);
+// const center = [width/2, height/2];
+//
+// // my variables
+// var rotationTimer;
+//
+// // Define color scale
+// var color = d3.scaleLinear()
+//     .domain([1, 20])
+//     .clamp(true)
+//     .range(['#fff', '#409A99']);
+//
+// // Get province name
+// function nameFn(d){
+//     return d && d.properties ? d.properties.ADMIN : null;
+// }
+//
+// // Get province name length
+// function nameLength(d){
+//     var n = nameFn(d);
+//     return n ? n.length : 0;
+// }
+//
+// // Get province color
+// function fillFn(d){
+//     return color(nameLength(d));
+// }
+//
+//
+// function mouseover(d){
+//     // Highlight hovered province
+//     // disableRotation();
+//     d3.select(this).style('fill', 'orange');
+//
+// }
+//
+// function mouseout(d){
+//     // Reset province color
+//     // enableRotation()
+//     svg.selectAll('path')
+//         // TODO fix styling
+//         .style('fill', '#e5e5e5'); // function(d){return fillFn(d);});
+// }
+//
+//
+// drawGlobe();
+// drawGraticule();
+// enableRotation();
+//
+// function drawGlobe() {
+//     d3.queue()
+//         .defer(d3.json, './assets/js/world-110m.json')
+//         .defer(d3.json, './assets/js/countries.geo.json')
+//         .await((error, worldData, locationData) => {
+//             svg.selectAll(".segment")
+//                 .data(topojson.feature(worldData, worldData.objects.countries).features)
+//                 .enter().append("path")
+//                 .attr("class", "segment")
+//                 .attr("d", path)
+//                 .style("stroke", "#888")
+//                 .style("stroke-width", "1px")
+//                 .style("fill", (d, i) => '#e5e5e5')
+//                 .style("opacity", ".7")
+//                 .on('mouseover', mouseover)
+//                 .on('mouseout', mouseout);
+//             // locations = locationData;
+//             // var features = countries[0].features;
+//             //
+//             // // Update color scale domain based on data
+//             // color.domain([0, d3.max(features, nameLength)]);
+//             //
+//             // // Draw each province as a path
+//             // svg.selectAll('path')
+//             //     .data(features)
+//             //     .enter().append('path')
+//             //     .attr('d', path)
+//             //     .attr('vector-effect', 'non-scaling-stroke')
+//             //     .style('fill', fillFn)
+//                 // .on('mouseover', mouseover)
+//                 // .on('mouseout', mouseout)
+//             // .on('click', clicked);
+//         });
+// }
+//
+//
+// function drawGraticule() {
+//     const graticule = d3.geoGraticule()
+//         .step([10, 10]);
+//
+//     svg.append("path")
+//         .datum(graticule)
+//         .attr("class", "graticule")
+//         .attr("d", path)
+//         .style("fill", "#fff")
+//         .style("stroke", "#ccc");
+// }
+//
+//
+// function enableRotation() {
+//     rotationTimer = d3.timer(function (elapsed) {
+//         projection.rotate([config.speed * elapsed - 120, config.verticalTilt, config.horizontalTilt]);
+//         svg.selectAll("path").attr("d", path);
+//     });
+// }
+//
+// function disableRotation() {
+//     rotationTimer.stop();
+// }
+//
+//
+//
