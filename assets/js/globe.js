dd18c40c3006edf0c805ba17f6af05a4c440e3ac
Thomasjkeel <thomasjames.keel@googlemail.com>
2019-05-13 11:13:12 +0100

    remove unwanted files

--- assets/js/globe.js
+++ assets/js/globe.js
@@ -1,90 +1,208 @@
-/*
-* Contains functions needed for the globe
-*/
+// adapted from: https://jorin.me/d3-canvas-globe-hover/
+// Configuration
 
-var osm = new og.layer.XYZ("OpenStreetMap", {
-    // specular: [0.0003, 0.00012, 0.00001],
-    // shininess: 20,
-    // diffuse: [0.89, 0.9, 0.83],
-    // extent: [[-130, 21], [-87, 58]],
-    isBaseLayer: true,
-    url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-    visibility: true,
-    attribution: 'Data @ OpenStreetMap contributors, ODbL'
-});
+// ms to wait after dragging before auto-rotating
+var rotationDelay = 3000
+// scale of the globe (not the canvas element)
+var scaleFactor = 0.9
+// autorotation speed
+var degPerSec = 6
+// start angles
+var angles = { x: -20, y: 40, z: 0}
+// colors
+var colorWater = '#fff'
+var colorLand = '#111'
+var colorGraticule = '#ccc'
+var colorCountry = '#a00'
 
-var globus = new og.Globe({
-    "target": "globus",
-    "name": "Earth",
-    "layers": [osm],
-    "sun": {
-        "active": false // deactivate the sun
-    },
-    'controls': [
-        new og.control.MouseNavigation({ autoActivate: false }),
-        new og.control.KeyboardNavigation({ autoActivate: true }),
-        // new og.control.EarthCoordinates({ autoActivate: true, center: false }),
-        new og.control.LayerSwitcher({ autoActivate: false }),
-        new og.control.ZoomControl({ autoActivate: true })//,
-        // new og.control.TouchNavigation({ autoActivate: false })
-    ]
-});
-globus.planet.setRatioLod(0.8) // low quality
+// Handler
 
-
-// add countries (Geo)JSON file with outline of countries
-var countriesLayer = new og.layer.Vector("Countries", {
-    'visibility': true,
-    'isBaseLayer': false,
-    'diffuse': [0, 0, 0],
-    'ambient': [1, 1, 1]
-});
-
-countriesLayer.addTo(globus.planet);
-
-// style the layer and zoom to extent on click
-var f = countries[0].features;
-for (var i = 0; i < f.length; i++) {
-    var fi = f[i];
-    countriesLayer.add(new og.Entity({
-        'geometry': {
-            'type': fi.geometry.type,
-            'coordinates': fi.geometry.coordinates,
-            'style': {
-                'fillColor': "rgba(255,255,255,0.6)"
-            }
-        },
-        'properties' : {
-            'name' : fi.properties.name
-        }
-    }));
+function enter(country) {
+    current.text(country && country.properties.name || 'Please Hover Over a Country')
 }
 
-countriesLayer.events.on("mouseleave", function (e) {
-    e.pickingObject.geometry.setFillColor(1, 1, 1, 0.6);
-    e.pickingObject.geometry.setLineColor(0.2, 0.6, 0.8, 1.0);
-});
-countriesLayer.events.on("mouseenter", function (e) {
-    e.pickingObject.geometry.bringToFront();
-    e.pickingObject.geometry.setFillColor(1, 0, 0, 0.4);
-    e.pickingObject.geometry.setLineColor(1, 0, 0, 1.0);
-});
-countriesLayer.events.on("lclick", function (e) {
-    globus.planet.flyExtent(e.pickingObject.geometry.getExtent());
-});
-countriesLayer.events.on("touchstart", function (e) {
-    globus.planet.flyExtent(e.pickingObject.geometry.getExtent());
-});
+function leave(country) {
+    current.text('Please Hover Over a Country')
+}
 
-// box in top right corner
-countriesLayer.events.on("mousemove", function (e) {
-    if (e.pickingObject) {
-        document.getElementById("layerName").innerHTML = e.pickingObject.properties.name;
-    } else {
-        document.getElementById("layerName").innerHTML = "";
+// Variables
+
+var current = d3.select('#current'),
+    canvas = d3.select('#globe'),
+    context = canvas.node().getContext('2d'),
+    water = {type: 'Sphere'},
+    projection = d3.geoOrthographic().precision(0.1),
+    graticule = d3.geoGraticule10(),
+    path = d3.geoPath(projection).context(context),
+    v0, // Mouse position in Cartesian coordinates at start of drag gesture.
+    r0, // Projection rotation as Euler angles at start.
+    q0, // Projection rotation as versor at start.
+    lastTime = d3.now(),
+    degPerMs = degPerSec / 1000,
+    width, height,
+    land, countries,
+    countryList,
+    autorotate, now, diff, rotation,
+    currentCountry;
+
+// Functions
+
+function setAngles() {
+    var rotation = projection.rotate()
+    rotation[0] = angles.y
+    rotation[1] = angles.x
+    rotation[2] = angles.z
+    projection.rotate(rotation)
+}
+
+function scale() {
+    width = document.documentElement.clientWidth
+    height = document.documentElement.clientHeight
+    canvas.attr('width', width).attr('height', height)
+    projection
+        .scale((scaleFactor * Math.min(width, height)) / 2)
+        .translate([width / 2, height / 2])
+    render()
+}
+
+function startRotation(delay) {
+    autorotate.restart(rotate, delay || 0)
+}
+
+function stopRotation() {
+    autorotate.stop()
+}
+
+function dragstarted() {
+    v0 = versor.cartesian(projection.invert(d3.mouse(this)))
+    r0 = projection.rotate()
+    q0 = versor(r0)
+    stopRotation()
+}
+
+function dragged() {
+    var v1 = versor.cartesian(projection.rotate(r0).invert(d3.mouse(this)))
+    var q1 = versor.multiply(q0, versor.delta(v0, v1))
+    var r1 = versor.rotation(q1)
+    projection.rotate(r1)
+    render()
+}
+
+function dragended() {
+    startRotation(rotationDelay)
+}
+
+function render() {
+    context.clearRect(0, 0, width, height)
+    fill(water, colorWater)
+    stroke(graticule, colorGraticule)
+    fill(land, colorLand)
+    if (currentCountry) {
+        fill(currentCountry, colorCountry)
     }
-});
+}
+
+function fill(obj, color) {
+    context.beginPath()
+    path(obj)
+    context.fillStyle = color
+    context.fill()
+}
+
+function stroke(obj, color) {
+    context.beginPath()
+    path(obj)
+    context.strokeStyle = color
+    context.stroke()
+}
+
+function rotate(elapsed) {
+    now = d3.now()
+    diff = now - lastTime
+    if (diff < elapsed) {
+        rotation = projection.rotate()
+        rotation[0] += diff * degPerMs
+        projection.rotate(rotation)
+        render()
+    }
+    lastTime = now
+}
+
+function loadData(cb) {
+    d3.json('./assets/js/world-110m.json', function(error, world) {
+        if (error) throw error
+        // d3.tsv('https://gist.githubusercontent.com/mbostock/4090846/raw/07e73f3c2d21558489604a0bc434b3a5cf41a867/world-country-names.tsv', function(error, countries) {
+        //     if (error) throw error
+        cb(world, countries)
+        // })
+    })
+}
+
+// https://github.com/d3/d3-polygon
+function polygonContains(polygon, point) {
+    var n = polygon.length
+    var p = polygon[n - 1]
+    var x = point[0], y = point[1]
+    var x0 = p[0], y0 = p[1]
+    var x1, y1
+    var inside = false
+    for (var i = 0; i < n; ++i) {
+        p = polygon[i], x1 = p[0], y1 = p[1]
+        if (((y1 > y) !== (y0 > y)) && (x < (x0 - x1) * (y - y1) / (y0 - y1) + x1)) inside = !inside
+        x0 = x1, y0 = y1
+    }
+    return inside
+}
+
+function mousemove() {
+    var c = getCountry(this)
+    if (!c) {
+        if (currentCountry) {
+            leave(currentCountry)
+            currentCountry = undefined
+            render()
+        }
+        return
+    }
+    if (c === currentCountry) {
+        return
+    }
+    currentCountry = c
+    render()
+    enter(c)
+}
+
+function getCountry(event) {
+    var pos = projection.invert(d3.mouse(event));
+    return countries[0].features.find(function(f) {
+        return f.geometry.coordinates.find(function(c1) {
+            return polygonContains(c1, pos) || c1.find(function(c2) {
+                return polygonContains(c2, pos)
+            })
+        })
+    })
+}
 
 
-// fit to extent of json
-// globus.planet.flyExtent(countriesLayer.getExtent());
+// Initialization
+
+setAngles()
+
+canvas
+    .call(d3.drag()
+        .on('start', dragstarted)
+        .on('drag', dragged)
+        .on('end', dragended)
+    )
+    .on('mousemove', mousemove)
+
+loadData(function(world, cList) {
+    land = topojson.feature(world, world.objects.land)
+    // countries = topojson.feature(world, world.objects.countries)
+    // countries is pre-defined in countries.js
+    countryList = cList
+
+    window.addEventListener('resize', scale)
+    scale()
+    autorotate = d3.timer(rotate)
+})
