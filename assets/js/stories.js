acd789579a296c9c9d82cb171178f813e57f1d6a
Thomasjkeel <thomasjames.keel@googlemail.com>
2019-05-20 12:12:23 +0100

    add example link

--- assets/js/stories.js
+++ assets/js/stories.js
@@ -1,3 +1,3 @@
 function getFirstPageStory() {
-    document.getElementById('page1_innertext').innerHTML = "Hello this is the air pollution story"
+    document.getElementById('page1_innertext').innerHTML = "Hello this is the air <a href='http://www.bbc.com'>LINK</a> pollution story"
 }
