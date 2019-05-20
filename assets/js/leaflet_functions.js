ef561e2877e57a4a6a9a139d4559f89b3acc912e
Thomasjkeel <thomasjames.keel@googlemail.com>
2019-05-20 21:18:43 +0100

    graph

--- assets/js/leaflet_functions.js
+++ assets/js/leaflet_functions.js
@@ -33,8 +33,8 @@ function subsetAirQualityData(data, qv) {
     });
     return subsetData
 }
-var layerMarkers = L.layerGroup([]);
 
+var layerMarkers = L.layerGroup([]);
 
 // TODO: make transfer of load data functions
 function loadNewData() {
@@ -93,14 +93,27 @@ function loadAirQualityData(qVal) {
                 fillOpacity: changeOpacity(row['properties']['aqi'])
             };
 
-            let marker = L.circleMarker([lat, lng], geojsonMarkerOptions).bindPopup("<div id='graphpopup'>Station ID: " + row['properties']['station'] +"The Air Quality is: "+row['properties']['aqi']+" AQI <br><button onclick='mymap.setView([51.1, 0.12], 6);';>A Button</button>Bar graph</div>");
+            let marker = L.circleMarker([lat, lng], geojsonMarkerOptions).bindPopup("<div id='graphpopup'>Station ID: " + row['properties']['station'] +"The Air Quality is: "+row['properties']['aqi']+" AQI <br><button onclick='mymap.setView([51.1, 0.12], 6);';>A Button</button><div id='graphHere"+ row['properties']['aqi'] + "'>The Graph Goes Here</div></div>");
+            marker.on('click', onMarkerClick );
+            function onMarkerClick(e) {
+                var popup = e.target.getPopup();
+                var chart_div = document.getElementById("graphpopup");
+                popup.setContent( chart_div );
+
+                let uniqId = $('#graphpopup > div')[0];
+                // console.log($('#graphpopup > div')[0]) // get all divs below
+                uniqId.innerHTML = ""
+                uniqId = "#" + uniqId.id.toString();
+
+                drawLineChart(14, "Shanghai", uniqId)
+            }
             layerMarkers.addLayer(marker);
         });
     });
     mymap.addLayer(layerMarkers);
 }
 
-function drawLineChart(date, cityname){
+function drawLineChart(date, cityname, divID){
     // parse the date / time
     var parseTime = d3.timeParse("%H");
 
@@ -138,7 +151,7 @@ function drawLineChart(date, cityname){
     }
 
 
-    var fields = ["aqi", "pm10", "pm25", "no2", "co", "so2", "o3"];
+    var fields = ["aqi"]//, "pm10", "pm25", "no2", "co", "so2", "o3"];
 
     // Check the time of data
     if (airQuality[0]["time"] === airQuality[1]["time"]){
@@ -150,12 +163,12 @@ function drawLineChart(date, cityname){
     airQuality.forEach(function(element){
         element["hour"] = parseTime(element["hour"]);
         element["aqi"] = +element["aqi"];
-        element["pm10"] = +element["pm10"];
-        element["pm25"] = +element["pm25"];
-        element["no2"] = +element["no2"];
-        element["co"] = +element["co"];
-        element["so2"] = +element["so2"];
-        element["o3"] = +element["o3"];
+        // element["pm10"] = +element["pm10"];
+        // element["pm25"] = +element["pm25"];
+        // element["no2"] = +element["no2"];
+        // element["co"] = +element["co"];
+        // element["so2"] = +element["so2"];
+        // element["o3"] = +element["o3"];
     });
 
     // Sort the data according to GMT(BST)
@@ -189,7 +202,7 @@ function drawLineChart(date, cityname){
             // append the svg object to the body of the page
             // appends a 'group' element to 'svg'
             // moves the 'group' element to the top left margin
-            var svg = d3.select("graphpopup").append("svg")
+            var svg = d3.select(divID).append("svg")
                 .attr("width", width + margin.left + margin.right)
                 .attr("height", height + margin.top + margin.bottom)
                 .append("g")
