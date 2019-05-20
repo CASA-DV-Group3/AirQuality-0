6a1e1cb7a118d9a9ff0c3ee5d21a19d7691446b1
Thomasjkeel <thomasjames.keel@googlemail.com>
2019-05-20 21:20:28 +0100

    adjust styling of popup

--- assets/js/leaflet_functions.js
+++ assets/js/leaflet_functions.js
@@ -93,7 +93,7 @@ function loadAirQualityData(qVal) {
                 fillOpacity: changeOpacity(row['properties']['aqi'])
             };
 
-            let marker = L.circleMarker([lat, lng], geojsonMarkerOptions).bindPopup("<div id='graphpopup'>Station ID: " + row['properties']['station'] +"The Air Quality is: "+row['properties']['aqi']+" AQI <br><button onclick='mymap.setView([51.1, 0.12], 6);';>A Button</button><div id='graphHere"+ row['properties']['aqi'] + "'>The Graph Goes Here</div></div>");
+            let marker = L.circleMarker([lat, lng], geojsonMarkerOptions).bindPopup("<div id='graphpopup'>Station ID: " + row['properties']['station'] +"The Air Quality is: "+row['properties']['aqi']+" AQI <br><br><div id='graphHere"+ row['properties']['aqi'] + "'>The Graph Goes Here</div><br><button onclick='mymap.setView([51.1, 0.12], 6);';>Load more data</button></div>");
             marker.on('click', onMarkerClick );
             function onMarkerClick(e) {
                 var popup = e.target.getPopup();
