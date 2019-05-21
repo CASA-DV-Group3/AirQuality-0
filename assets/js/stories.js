44c673ac963038922c0f20686620e159e5b5841b
Thomasjkeel <thomasjames.keel@googlemail.com>
2019-05-21 13:14:37 +0100

    add link

--- assets/js/stories.js
+++ assets/js/stories.js
@@ -1,4 +1,4 @@
-allText = {"1": "\"Hello this is the air <a href='#ab'>LINK</a> pollution story\"", "2": "NEW this is the air <a href='http://www.bbc.com'>LINK</a> pollution story"}
+allText = {"1": "\"Hello this is the air <a href='#page2'>LINK</a> pollution story\"", "2": "NEW this is the air <a href='http://www.bbc.com'>LINK</a> pollution story"}
 
 function getStory(id) {
 
