e53672bd499fba7decffd16ea4dbcaef9bc35842
Thomasjkeel <thomasjames.keel@googlemail.com>
2019-05-25 19:42:11 +0100

    add play fucntion

--- assets/js/slidersAndButtons.js
+++ assets/js/slidersAndButtons.js
@@ -132,17 +132,17 @@ L.easyButton({
     states: [{
         stateName: 'unplayed',        // name the state
         icon:      '&#x25b6;',               // and define its properties
-        title:     'Play',      // like its title
+        title:     'Play Analysis',      // like its title
         onClick: function(btn, map) {       // and its callback
-            console.log("Hello");
+            document.getElementById('infoDiv').innerText = "Hello There";
             btn.state('played');
         }
     }, {
         stateName: 'played',
         icon:      'fa-pause',
-        title:     'Pause',
+        title:     'Pause Analysis',
         onClick: function(btn, map) {
-            console.log("Hello");
+            document.getElementById('infoDiv').innerHTML = '<h3>Text about the map</h3><br><h4>This will reload when changing points</h4><br><button class="btn btn-outline-info btn-sm" onclick="mymap.setView(new L.LatLng(40.737, -73.923), 8);;">Zoom to Layer</button><button class="btn btn-outline-info btn-sm">Left</button><button class="btn btn-outline-info btn-sm">Right</button>';
             btn.state('unplayed');
         }
     }]
