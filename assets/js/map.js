4e41d6b161298f6462a23656b003d28ee3e14467
Thomasjkeel <thomasjames.keel@googlemail.com>
2019-05-26 21:55:21 +0100

    add data ref

--- assets/js/map.js
+++ assets/js/map.js
@@ -24,8 +24,8 @@ function addMapControls () {
 
     info.onAdd = function(map) {
         this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
-        this._div.id = "infoDiv"
-        this._div.style.marginBottom = "25%";
+        this._div.id = "infoDiv";
+        this._div.style.marginBottom = "30%";
         this._div.style.textAlign = "left";
         this.update();
         return this._div;
@@ -33,10 +33,12 @@ function addMapControls () {
 
 // method that we will use to update the control based on feature properties passed
     info.update = function (props) {
-        this._div.innerHTML = '<h3>Global Air Quality Monitoring</h3><br>' +
-            '<h5>To load the mointoring station data click on one of the industrial grouping buttons on the left-hand side</h5><br>' +
+        this._div.innerHTML = '<h3>Global Air Quality Monitoring Stations</h3>' +
+            '<h5 class="text-light" style="text-align: left; font-size: 0.9rem"><i>\'Making the invisible visible\'</i></h5>' +
+            '<p class="text-light">For this visualisation, the mointoring station data click on one of the industrial grouping (World Bank, 2017) buttons on the left-hand side</p><br>' +
             'Air Quality Index (AQI) is used in this for an explanation of AQI groupings, visit <a id="aqiHover" href="#d" onclick="window.open(\'https://www.ourair.org/sbc/the-air-quality-index/\')"><b>AQI index</b></a>' +
             ''
+
     };
 
     legend.addTo(mymap); // add the legend
