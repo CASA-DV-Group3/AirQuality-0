2b668b1dd58dbf24fc3a57e8b6d3e19d3e477a72
Thomasjkeel <thomasjames.keel@googlemail.com>
2019-05-26 22:18:15 +0100

    finish map

--- assets/js/map.js
+++ assets/js/map.js
@@ -33,12 +33,13 @@ function addMapControls () {
 
 // method that we will use to update the control based on feature properties passed
     info.update = function (props) {
-        this._div.innerHTML = '<h3>Global Air Quality Monitoring Stations</h3>' +
+        this._div.innerHTML = '<h3>Global Air Quality Monitoring</h3>' +
             '<h5 class="text-light" style="text-align: left; font-size: 0.9rem"><i>\'Making the invisible visible\'</i></h5>' +
-            '<p class="text-light">For this visualisation, live data from air quality monitoring stations across the world can be loaded onto the map using the buttons on the left-hand side.' +
-            'The buttons are divided into three groups of % GDP that is accounted for by the industrial sector, which serve as a proxy for countries we expect to pollute more (World Bank, 2017). ' +
+            '<p class="text-light">For this visualisation, live data from air quality monitoring stations across the world can be loaded onto the map using the buttons on the left.' +
+            'These buttons are divided into three groups of % GDP that is accounted for by the industrial sector, which serve as a proxy for countries we expect to pollute more (World Bank, 2017). ' +
             'The data itself is a measure of the Air Quality Index (AQI) in a location, which contains a calculation determining the health <a href="#" class="text-light" data-toggle="tooltip" data-placement="top" title="Broadly determined by the WHO: Air Quality Guidelines set out in 2005"><u>risk</u></a>' +
-            ' associated with the local concentrations of air pollutants (read more <a id="aqiHover" href="#d" onclick="window.open(\'https://www.ourair.org/sbc/the-air-quality-index/\')"><b>here</b></a>). '
+            ' associated with the local concentrations of air pollutants (read more <a id="aqiHover" href="#d" onclick="window.open(\'https://www.ourair.org/sbc/the-air-quality-index/\')"><b>here</b></a>). ' +
+            '<i>Click</i> on a monitoring station to view the last 24 hours worth of measurements.'
     };
 
     legend.addTo(mymap); // add the legend
