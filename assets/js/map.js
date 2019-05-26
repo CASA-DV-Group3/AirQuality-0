fda61d0f910ccc1c69639501ee07e4d345e525a6
Thomasjkeel <thomasjames.keel@googlemail.com>
2019-05-26 22:19:34 +0100

    finish map

--- assets/js/map.js
+++ assets/js/map.js
@@ -39,7 +39,7 @@ function addMapControls () {
             'These buttons are divided into three groups of % GDP that is accounted for by the industrial sector, which serve as a proxy for countries we expect to pollute more (World Bank, 2017). ' +
             'The data itself is a measure of the Air Quality Index (AQI) in a location, which contains a calculation determining the health <a href="#" class="text-light" data-toggle="tooltip" data-placement="top" title="Broadly determined by the WHO: Air Quality Guidelines set out in 2005"><u>risk</u></a>' +
             ' associated with the local concentrations of air pollutants (read more <a id="aqiHover" href="#d" onclick="window.open(\'https://www.ourair.org/sbc/the-air-quality-index/\')"><b>here</b></a>). ' +
-            '<i>Click</i> on a monitoring station to view the last 24 hours worth of measurements.'
+            '<i>Click</i> on a monitoring station to view the information about it and past measurements.'
     };
 
     legend.addTo(mymap); // add the legend
